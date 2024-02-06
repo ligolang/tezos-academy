0c27215460965a3b2e231a11ba53b3b8ca4158e0
lykimq <lykimq@gmail.com>
2024-02-06 12:52:25 +0100

    Chapter_about: correct some small grammar

--- src/frontend/src/pages/ChapterAbout/ChapterAbout.data.tsx
+++ src/frontend/src/pages/ChapterAbout/ChapterAbout.data.tsx
@@ -2,19 +2,19 @@ export const data = `#Chapter 1 : Tezos Academy
 
 <dialog character="admiral">Hello rookie! I'm admiral Adama. I hope you are ready for a fight. The Xenomorphs have been attacking our colonies for days now. We are throwing everything we’ve got at them, that means even you rookie, so you better learn and fast! You have been assigned a ship, go ahead and get prepared.</dialog>
 
-Tezos Academy is a fun interactive tutorial on how to code smart contracts for <a href="https://tezos.com/" target="_blank">Tezos</a>. You are about to go an an adventure filled with smart contracts!
+Tezos Academy is a fun interactive tutorial on how to code smart contracts for <a href="https://tezos.com/" target="_blank">Tezos</a>. You are about to go on an adventure filled with smart contracts!
 
-By default, Tezos smart contracts are written in <a href="https://tezos.gitlab.io/whitedoc/michelson.html" target="_blank">Michelson</a>, but it is an hard to learn low level formal language. For this tutorial, we will use <a href="https://ligolang.org/" target="_blank">LIGO</a> instead. The syntax is high level and transpiles to Michelson.
+By default, Tezos smart contracts are written in <a href="https://tezos.gitlab.io/whitedoc/michelson.html" target="_blank">Michelson</a>, but it is a hard-to-learn, low-level formal language. For this tutorial, we will use <a href="https://ligolang.org/" target="_blank">LIGO</a> instead. The syntax is high-level and transpiles to Michelson.
 
 ## Objectives
 
 In this tutorial, we will go over each fundamental principles of LIGO while coding different smart contracts with a sci-fi narrative.
 
-The first 20 chapters are easy and made for you to learn without too much hassle. The following 10 chapters are much harder and made for you to experience real combat, huh I mean real smart contract development on tezos!
+The first 20 chapters are easy and made for you to learn without too much hassle. The following 10 chapters are much harder and made for you to experience real combat, huh, I mean real smart contract development on Tezos!
 
-Each chapter is made of a theorical part and exercice. Code the exercise in the online editor and click validate to compare your code against the expected solution. Differences will be highlighted is a git-diff view.
+Each chapter is made of a theorical part and an exercice. Code the exercise in the online editor and click "validate" to compare your code against the expected solution. Differences will be highlighted in a git-diff view.
 
 <img class="img-git-diff" alt="editor" src="/images/editor.png" />
 
-So go ahead soldier and make us proud!
+So go ahead, soldier, and make us proud!
 `
